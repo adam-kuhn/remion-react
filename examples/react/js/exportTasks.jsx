3ae60c39741f7b8ab9dc73dbf13c4ec7bdcfcb2f
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-16 02:08:42 -0600

    fully functioning download tasks

--- examples/react/js/exportTasks.jsx
+++ examples/react/js/exportTasks.jsx
@@ -9,10 +9,11 @@ var app = app || {};
       let csvContent = 'data:text/csv;charset=utf-8,'
       const todos = this.props.todos
       for (let todo of todos) {
-        if (todo.priority === 6) {
-          todo.priority = 'N/A'
-        }
-        let row = [todo.title, todo.completed, todo.priority]
+        // let priority = todo.priority
+        // if (priority === 6) {
+        //   priority = 'N/A'
+        // }
+        let row = [todo.title, todo.completed, todo.priority === 6 ? 'N/A' : todo.priority]
         rows.push(row)
       }
       rows.forEach(function (rowArray) {
