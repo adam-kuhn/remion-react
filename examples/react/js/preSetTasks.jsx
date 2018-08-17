35e9acad687c8955085afc7b0e2eec50aa5582a5
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-17 13:18:12 +0300

    functional with API call

--- examples/react/js/preSetTasks.jsx
+++ examples/react/js/preSetTasks.jsx
@@ -1,10 +1,3 @@
-//     componentDidMount: function () {
-//       // fetch('https://storage.googleapis.com/remion-rekry/tasks.json')
-//       //   .then(result => {
-//       //     console.log(result.json())
-//       //   })
-//       // this is what the API call will get...on a plane/no wifi
-
 var app = app || {};
 
 (function () {
@@ -19,30 +12,23 @@ var app = app || {};
     },
 
     componentDidMount: function () {
-      this.setState({
-        preSet: [
-          {
-            priority: 1,
-            isComplete: false,
-            text: 'Go to sleep'
-          },
-          {
-            priority: 5,
-            isComplete: false,
-            text: 'Clean up room'
-          },
-          {
-            priority: 3,
-            isComplete: false,
-            text: 'Go to shower'
-          },
-          {
-            priority: 5,
-            isComplete: false,
-            text: 'Eat something'
-          }
-        ]
-      })
+      fetch('https://storage.googleapis.com/remion-rekry/tasks.json')
+        .then(result => {
+          return result.json()
+        })
+        .then(data => {
+          const preSets = data.map((todo, idx) => {
+            return (
+              <button key={`presets${idx}`} type='button' onClick={this.selectTask}
+                value={todo.text}
+                data-complete={todo.isComplete}
+                data-priority={todo.priority}>{`Task: ${todo.text} Priority: ${todo.priority}`}</button>
+            )
+          })
+          this.setState({
+            preSets
+          })
+        })
     },
 
     openMenu: function () {
@@ -55,7 +41,7 @@ var app = app || {};
       const todos = this.props.todos
       const newTodo = {
         title: event.target.value,
-        id: event.target.value,
+        id: event.target.key,
         timeStamp: '',
         priority: Number(event.target.getAttribute('data-priority')),
         completed: event.target.getAttribute('data-complete') === 'true'
@@ -67,20 +53,12 @@ var app = app || {};
       })
     },
     render: function () {
-      const SetTasks = this.state.preSet.map((todo, idx) => {
-        return (
-          <button key={`presets${idx}`} type='button' onClick={this.selectTask}
-            value={todo.text}
-            data-complete={todo.isComplete}
-            data-priority={todo.priority}>{`Task: ${todo.text} Priority: ${todo.priority}`}</button>
-        )
-      })
       return (
         <div className='priority'>
           <button className='main-btn' type='button' onClick={this.openMenu}>Pre Set</button>
           {this.state.showMenu &&
             <div className='drop-down'>
-              {SetTasks}
+              {this.state.preSets}
             </div>}
         </div>
       )
